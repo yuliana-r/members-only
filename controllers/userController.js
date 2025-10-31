053a049867f3e754ee3aea585a7a8287bf8683c8
Yuliana R <iuliana.rotaru234@gmail.com>
2025-10-31 20:22:59 +0000

    Add confirm delete account view and functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,22 @@
 const db = require("../db/queries/user_queries");
 
 //GET /users/:userId/delete
-exports.getDeleteUserConfirmation = async (req, res) => {};
+exports.getDeleteUserConfirmation = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await db.getUserById(userId);
+
+    if (!user) {
+      const err = new Error("User not found!");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("confirm_delete_user", { title: "confirm delete user", user });
+  } catch (error) {
+    next(error);
+  }
+};
 
 //POST /users/:userId/delete
 exports.deleteUser = async (req, res) => {
